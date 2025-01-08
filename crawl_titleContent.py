c3ae2af0b3ad4a006d3cd3d9a8564c33780add66
Kalvin <yaojaikai520@gmail.com>
2025-01-08 23:31:52 +0800

    re

--- crawl_titleContent.py
+++ crawl_titleContent.py
@@ -7,7 +7,8 @@ url = "https://spa1.scrape.center/"
 
 # 降低被拒絕訪問顧增加headers設定
 request = req.Request(url, headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"})
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 "
+                  "Safari/537.36"})
 
 # 處理中文字,避免中文出現亂碼
 with req.urlopen(request) as response:
